aaef52f75b540b77052a2efb1ec07d344cd5a603
alessandroscarlatti <violanotesnoextras@gmail.com>
2019-01-09 18:11:05 -0600

    initial commit

---
+++ src/main/java/com/scarlatti/springbatchdemobase/SpringBatchDemoBaseApplication.java
@@ -0,0 +1,14 @@
+package com.scarlatti.springbatchdemobase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchDemoBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchDemoBaseApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/scarlatti/springbatchdemobase/SpringBatchDemoBaseApplicationTests.java
@@ -0,0 +1,17 @@
+package com.scarlatti.springbatchdemobase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBatchDemoBaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
